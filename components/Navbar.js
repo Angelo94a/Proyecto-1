682d87b1339642a9c788882a1754ac0b36f7333f
Angelo Almeida <angelo94.aa@gmail.com>
2022-12-11 09:07:23 -0500

    actualiszando navbar 11/12/2022

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,49 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 import style from '/style/navbar.module.scss'
-export default function Navbar() {
-  return (
-    <>
-      <header className={style.hero}>
-        <div className={style.container}>
-          <nav className={style.nav}>
-            <h1 className={style.nav__logo}>Golden<span>Gym</span></h1>
-            <ul className={style.nav__menu}>
-              <li className={style.nav__item}>
-                <Link href="#" className={style.nav__link}>item-1</Link>
-              </li>
-              <li className={style.nav__item}>
-                <Link href="#" className={style.nav__link}>item-2</Link>
-              </li>
-              <li className={style.nav__item}>
-                <Link href="#" className={style.nav__link}>item-3</Link>
-              </li>
-            </ul>
 
-            <div className={style.btn}>
-              <span className={style.bar}></span>
-              <bar-1></bar-1>
-              <span className={style.bar}></span>
-              <bar-2></bar-2>
-              <span className={style.bar}></span>
-              <bar-3></bar-3>
-            </div>
-          </nav>
-        </div>
-      </header>
-    </>
-  )
-};
+const link = [
+	{
+		label: 'Home',
+		route: '/',
+	},
+	{
+		label: 'Contacs',
+		route: '/contacs',
+	},
+	{
+		label: 'About',
+		route: '/about',
+	},
+]
+export default function Navbar() {
+	return (
+		<>
+			<header className={style.hero}>
+				<div className={style.container}>
+					<nav className={style.nav}>
+						<h1 className={style.nav__logo}>
+							Golden<span>Gym</span>
+						</h1>
+						<ul className={style.nav__menu}>
+							{link.map(({ label, route }) => (
+								<li key={route} className={style.nav__item}>
+									<Link href={route} className={style.nav__link}>{label}</Link>
+								</li>
+							))}
+						</ul>
+
+						<div className={style.btn}>
+							<span className={style.bar}></span>
+							<bar-1></bar-1>
+							<span className={style.bar}></span>
+							<bar-2></bar-2>
+							<span className={style.bar}></span>
+							<bar-3></bar-3>
+						</div>
+					</nav>
+				</div>
+			</header>
+		</>
+	)
+}
