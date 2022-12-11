727c824e00f7d64cdafacecf09ff77eb3bb2cc86
Angelo Almeida <angelo94.aa@gmail.com>
2022-12-11 11:55:01 -0500

    agregando layout

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,20 @@ import '../style/global.css'
 import Navbar from '../components/Navbar'
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <head />
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  )
+		<html lang="es">
+			<head />
+			<body>
+				<Navbar />
+				{children}
+				<script
+					type="module"
+					src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+				></script>
+				<script
+					nomodule
+					src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+				></script>
+			</body>
+		</html>
+	)
 };
